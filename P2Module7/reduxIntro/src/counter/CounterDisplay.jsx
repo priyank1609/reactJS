84c9d148c841e0f54ed208a51f2eeb421b726e27
priyank1609 <sh.priyank1999@gmail.com>
2026-03-05 17:12:53 +0530

    doe

--- P2Module7/reduxIntro/src/counter/CounterDisplay.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux'
-
-const CounterDisplay = () => {
-    const count = useSelector(state => state.counter.count);
-
-    return <p className="text-4xl">{count}</p>;
-}
-
-export default CounterDisplay
