5d8346d3da8a68ac77a2026af48e707d32e67232
priyank1609 <sh.priyank1999@gmail.com>
2026-03-05 14:09:13 +0530

    finalize

--- P2Module7/reduxIntro/src/router.js
+++ P2Module7/reduxIntro/src/router.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Dashboard from "./Dashboard";
 import Counter from "./counter/Counter";
-import ShoppingPage from "./shopping/ShoppingPage";
 
 const router = createBrowserRouter([
   {index: true, Component: Dashboard},
